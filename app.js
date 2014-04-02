185826c68463b11504833f50a286c186fa4d8403
ltfschoen <ltfschoen@gmail.com>
2014-04-02 18:34:39 +1100

    Added Userbin and Testing MongoDB

--- app.js
+++ app.js
@@ -1,50 +1,64 @@
-
 /**
  * Module dependencies.
  */
 
 // tie variables to packages, dependencies, node functionalities, and routes
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+// link to Mongo Labs database
 
-// instantiates Express engine and assigns app variables to it
-var app = express();
+// module.exports = function (flights) {
+	var express = require('express');
 
-// all environments
-// sets port
-app.set('port', process.env.PORT || 3000);
-// defines locations of views
-app.set('views', path.join(__dirname, 'views'));
-// defines Jade engine to render views
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+	// var routes = require('./routes')(flights);
+	var routes = require('./routes')
+	var user = require('./routes/user');
+	var http = require('http');
+	var path = require('path');
+	// userbin to hide secret keys
+	var userbin = require('userbin');
 
-// development only
-// allows error-checking during development
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+	// instantiates Express engine and assigns app variables to it
+	var app = express();
 
-// defines routes for URI's hit
-app.get('/', routes.index);
-// map to /routes/user.js and call 'list' function
-app.get('/users', user.list);
+	userbin.config({
+  		secret_key: 'YOUR_APP_ID'
+	});
 
-// map to dashboard URI /routes/dashboard.js
-app.get('/dashboard', routes.dashboard);
+	// all environments
+	// sets port
+	app.set('port', process.env.PORT || 3000);
+	// defines locations of views
+	app.set('views', path.join(__dirname, '/views'));
+	// defines engine to render views
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser('your secret here'));
+	app.use(userbin.authenticate());
+	app.use(express.session());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 
-// create http server and launch
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+	// development only
+	// allows error-checking during development
+	if ('development' == app.get('env')) {
+	  app.use(express.errorHandler());
+	}
+
+	// defines routes for URI's hit
+	app.get('/', routes.index);
+	// map to /routes/user.js and call 'list' function
+	app.get('/users', user.list);
+
+	// map to dashboard URI /routes/dashboard.js
+	app.get('/dashboard', routes.dashboard);
+
+	// create http server and launch
+	http.createServer(app).listen(app.get('port'), function(){
+	  console.log('Express server listening on port ' + app.get('port'));
+	});
+
+// 	return app;
+// }
---
+++ data/index.js
@@ -0,0 +1,10 @@
+// module.exports = {
+// 	"1": {
+// 		"number": 1,
+// 		"name": "Luke"
+// 	},
+// 	"2": {
+// 		"number": 2,
+// 		"name": "Joe"
+// 	}
+// };
